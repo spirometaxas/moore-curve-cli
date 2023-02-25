471b231a08af03046aae4fb99fe6b7f4eef2b91e
Spiro Metaxas <spirometaxas@gmail.com>
2023-02-25 12:41:35 -0500

    Version 1.2.0

--- src/cli.js
+++ src/cli.js
@@ -9,6 +9,7 @@ const printUsage = function(showIntro) {
     console.log('\n' + 
                 ' Usage:\n' + 
                 '   $ moore-curve-cli <n>\n' + 
+                '   $ moore-curve-cli <n> [options]\n' + 
                 '\n' +
                 '   <n> is the recursive step, a number greater than or equal to 1\n' + 
                 '\n' +
